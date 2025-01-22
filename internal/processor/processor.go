4cd357721684e1767737d8eedbec0ad3e93ce5d6
DrLivsey00 <emihrin50@gmail.com>
2025-01-22 14:38:42 +0200

    -

--- internal/processor/processor.go
+++ internal/processor/processor.go
@@ -2,6 +2,7 @@ package processor
 
 import (
 	"github.com/hyle-team/tss-svc/internal/api/common"
+	"github.com/hyle-team/tss-svc/internal/bridge"
 	bridgeTypes "github.com/hyle-team/tss-svc/internal/bridge/types"
 	database "github.com/hyle-team/tss-svc/internal/db"
 	"github.com/hyle-team/tss-svc/internal/types"
@@ -11,7 +12,7 @@ import (
 type Processor struct {
 	//  TODO: Add Core connector
 
-	client bridgeTypes.Client
+	clients Clientr
 	db     database.DepositsQ
 }
 
@@ -31,8 +32,41 @@ func (p *Processor) FetchDepositData(identifier *types.DepositIdentifier) (*data
 	depositData, err := p.client.GetDepositData(dbIdentifier)
 
 	if err != nil {
-		return nil, errors.Wrap(err, "get deposit data")
+		return nil, errors.Wrap(err, "failed get deposit data")
 	}
+
+	// TODO: Add fetching withdrawal token data from Core
+
+	//set deposit data to deposit structure
+	depositAmount := depositData.DepositAmount.String()
+
+	// TODO: This data has to be
+	withdrawalAmount := "123"
+
+
+//DepositIdentifier:  id,
+//	DestinationChainId: eventBody.Network,
+//		DestinationAddress: eventBody.Receiver,
+//		TokenAddress:       bridge.DefaultNativeTokenAddress,
+//		DepositAmount:      eventBody.Amount,
+//		Block:              int64(log.BlockNumber),
+//		SourceAddress:      from.String(),
+	deposit := &database.Deposit{
+		DepositIdentifier: dbIdentifier,
+		Depositor:         &depositData.SourceAddress,
+		DepositAmount:     &depositAmount,
+		DepositToken:      &depositData.TokenAddress,
+		Receiver:          &depositData.DestinationAddress,
+		WithdrawalToken:   ,
+		DepositBlock:      nil,
+		WithdrawalStatus:  0,
+		WithdrawalTxHash:  nil,
+		WithdrawalChainId: nil,
+		WithdrawalAmount:  nil,
+		IsWrappedToken:    nil,
+		Signature:         nil,
+	}
+
 	return
 
 }
