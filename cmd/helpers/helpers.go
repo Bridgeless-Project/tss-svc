33224d58fbcec000ce468c5ce892a4c0df9125bd
slbmax <slobodzian.maxim@gmail.com>
2025-02-12 15:43:04 +0200

    expanded helper commands

--- cmd/helpers/helpers.go
+++ cmd/helpers/helpers.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"github.com/hyle-team/tss-svc/cmd/helpers/generate"
+	"github.com/hyle-team/tss-svc/cmd/helpers/parse"
 	"github.com/spf13/cobra"
 )
 
@@ -16,5 +17,5 @@ var Cmd = &cobra.Command{
 
 func registerHelpersCommands(cmd *cobra.Command) {
 	cmd.AddCommand(generate.Cmd)
-	cmd.AddCommand(parsePubKeyCmd)
+	cmd.AddCommand(parse.Cmd)
 }
--- cmd/helpers/parse/address.go
+++ cmd/helpers/parse/address.go
@@ -1,4 +1,4 @@
-package helpers
+package parse
 
 import (
 	"crypto/elliptic"
@@ -12,9 +12,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var parsePubKeyCmd = &cobra.Command{
-	Use:   "parse-address [x-cord] [y-cord]",
-	Short: "Parse address from the given point",
+var parseAddressCmd = &cobra.Command{
+	Use:   "address [x-cord] [y-cord]",
+	Short: "Parse eth address from the given point",
 	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		xCord, ok := new(big.Int).SetString(args[0], 10)
@@ -28,11 +28,8 @@ var parsePubKeyCmd = &cobra.Command{
 		}
 
 		marshalled := elliptic.Marshal(tss.S256(), xCord, yCord)
-
-		// Marshalled point contains constant 0x04 first byte, we have to remove it
-		pubBytes := marshalled[1:]
-
-		hash := crypto.Keccak256(pubBytes)
+		// Marshalled point contains constant 0x04 first byte, we do not have to include it
+		hash := crypto.Keccak256(marshalled[1:])
 
 		// The Ethereum address is the last 20 bytes of the hash.
 		address := common.BytesToAddress(hash[12:]) // hash[12:32]
